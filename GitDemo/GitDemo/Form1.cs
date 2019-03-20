c0175deee3458459c080c8310d854daa2986a5b5
Patrick Gallant <pdgallant.pdg@gmail.com>
2019-03-20 10:18:29 -0300

    btn2

--- GitDemo/GitDemo/Form1.cs
+++ GitDemo/GitDemo/Form1.cs
@@ -21,5 +21,10 @@ namespace GitDemo
         {
             MessageBox.Show("Test");
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.PerformClick();
+        }
     }
 }
